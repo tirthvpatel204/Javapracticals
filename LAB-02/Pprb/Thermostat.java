591668aca18feeb7e9428740e0c4436806fe693f
tirthvpatel204 <pateltirth2048@gmail.com>
2026-07-18 00:04:33 +0530

    PR UPDATED !

--- LAB-02/Pprb/Thermostat.java
+++ LAB-02/Pprb/Thermostat.java
@@ -1,78 +1,85 @@
-class Thermostat {
+public class Thermostat {
 
+    // Instance variables
     private String location;
     private int temperature;
 
-    private static final int min=16;
-    private static final int max=30;
-    private static int actco=0;
-    Thermostat(String location, int startTemp) 
-    {
+    // Constants
+    private static final int MIN_TEMP = 16;
+    private static final int MAX_TEMP = 30;
+
+    // Static variable
+    private static int activeCount = 0;
+
+    // Constructor with location and temperature
+    public Thermostat(String location, int startTemp) {
         this.location = location;
 
-        if (startTemp>=min && startTemp<=max)
-             {
+        if (startTemp >= MIN_TEMP && startTemp <= MAX_TEMP) {
             this.temperature = startTemp;
         } else {
-            this.temperature = 22;
+            this.temperature = 22; // Default temperature
         }
 
-        actco++;
+        activeCount++;
     }
-    Thermostat(String location) 
-    {
+
+    // Constructor with default temperature
+    public Thermostat(String location) {
         this(location, 22);
     }
-    void raise() 
-    {
-        if (temperature<max) 
-        {
+
+    // Increase temperature
+    public void raise() {
+        if (temperature < MAX_TEMP) {
             temperature++;
-        } else 
-            {
+        } else {
             System.out.println(location + " is already at maximum temperature (" + MAX_TEMP + "°C)");
         }
     }
-    void lower() 
-    {
-        if (temperature>min) 
-        {
+
+    // Decrease temperature
+    public void lower() {
+        if (temperature > MIN_TEMP) {
             temperature--;
-        } else 
-        {
+        } else {
             System.out.println(location + " is already at minimum temperature (" + MIN_TEMP + "°C)");
         }
     }
-    void display() 
-    {
+
+    // Display current temperature
+    public void display() {
         System.out.println(location + " Temperature = " + temperature + "°C");
     }
-    static int getActiveCount() 
-    {
-        return actco;
+
+    // Return number of active thermostats
+    public static int getActiveCount() {
+        return activeCount;
     }
 
-    public static void main(String[] args) 
-    {
+    // Main method
+    public static void main(String[] args) {
 
         Thermostat hall = new Thermostat("Hall");
         Thermostat bedroom = new Thermostat("Bedroom", 25);
 
         System.out.println("Increasing Hall Temperature:");
-        for (int i=0; i<10; i++) 
-        {
+
+        for (int i = 0; i < 10; i++) {
             hall.raise();
             hall.display();
         }
 
         System.out.println("\nDecreasing Hall Temperature:");
-        for (int i=0; i<20; i++) 
-        {
+
+        for (int i = 0; i < 20; i++) {
             hall.lower();
             hall.display();
         }
+
         System.out.println("\nBedroom Status:");
         bedroom.display();
+
         System.out.println("\nActive Thermostats = " + Thermostat.getActiveCount());
     }
 }
