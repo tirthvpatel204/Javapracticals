5d2392516ca9e87bfe03b70c5d72443fc4affb9d
tirthvpatel204 <pateltirth2048@gmail.com>
2026-07-17 13:40:12 +0530

    Lab-02 1st pr has been successfull

--- LAB-02/Pprb/Thermostat.java
+++ LAB-02/Pprb/Thermostat.java
@@ -1,8 +1,65 @@
 class Thermostat 
 {
-    private String locat;
+    private String loca;
     private int temp;
-    private static final int min = 16;
-    private static final int max = 30;
-    private static int actcount = 0;
+    private static final int min=16;
+    private static final int max=30;
+    private static int actacount=0;
+
+    Thermostat(String loca, int startTemp) 
+    {
+        this.loca=loca;
+
+        if (startTemp>=min && startTemp<=max)
+            temp=startTemp;
+        else
+            temp=22;
+           actacount++;
+    }
+    Thermostat(String loca) 
+    {
+        this(loca, 22);
+    }
+
+    void raise() 
+    {
+        if (temp<max)
+            temp++;
+        else
+            System.out.println("Already at maximum (30)");
+    }
+
+    void lower() 
+    {
+        if (temp>min)
+            temp--;
+        else
+            System.out.println("Already at minimum (16)");
+    }
+
+    void print()
+    {
+        System.out.println(loca + " Temperature = " + temp);
+    }
+
+    static int getActiveCount() 
+    {
+        return actacount;
+    }
+    public static void main(String[] args)
+     {
+        Thermostat t1 = new Thermostat("Hall");
+        Thermostat t2 = new Thermostat("Bedroom", 25);
+        for (int i = 1; i <= 10; i++) 
+        {
+            t1.raise();
+            t1.print();
+        }
+        for (int i = 1; i <= 20; i++) 
+        {
+            t1.lower();
+            t1.print();
+        }
+        System.out.println("Active Thermostats = " + Thermostat.getActiveCount());
+    }
 }
